cca9732d050060d7bcb625e7f60a53e3947831b5
Stanislau Matskevich <st.matskevich.work@gmail.com>
2020-04-11 21:13:06 +0300

    Added watch for preload.js

--- public/electron.js
+++ public/electron.js
@@ -2,6 +2,8 @@ const { app, BrowserWindow } = require('electron')
 const path = require('path')
 const isDev = require('electron-is-dev');
 
+const chokidar = isDev ? require('chokidar') : undefined;
+
 function createWindow() {
     // Create the browser window.     
     win = new BrowserWindow({
@@ -12,9 +14,17 @@ function createWindow() {
         }
     })
 
+    //load from dev server or from builded file
     win.loadURL(isDev ?
         'http://localhost:3000' :
         `file://${path.join(__dirname, '../build/index.html')}`
     );
+
+    //watching preload.js
+    if (chokidar) {
+        chokidar.watch(path.join(__dirname, '../build/preload.js')).on('change', () => {
+            win.reload();
+        });
+    }
 }
 app.on('ready', createWindow)
