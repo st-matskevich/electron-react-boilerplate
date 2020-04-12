7af9b31372b423da07bbde3dab5017aacf1e76c8
Stanislau Matskevich <st.matskevich.work@gmail.com>
2020-04-12 23:12:23 +0300

    Reverted packing. Should keep this repo as simple as possible

--- public/electron.js
+++ public/electron.js
@@ -2,29 +2,20 @@ const { app, BrowserWindow } = require('electron')
 const path = require('path')
 const isDev = require('electron-is-dev');
 
-const chokidar = isDev ? require('chokidar') : undefined;
-
 function createWindow() {
     // Create the browser window.     
     win = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
-            preload: path.join(__dirname, '../build/preload.js'),
+            preload: path.join(__dirname, './preload.js'),
         }
     })
 
     //load from dev server or from builded file
     win.loadURL(isDev ?
         'http://localhost:3000' :
-        `file://${path.join(__dirname, '../build/index.html')}`
+        `file://${path.join(__dirname, './index.html')}`
     );
-
-    //watching preload.js
-    if (chokidar) {
-        chokidar.watch(path.join(__dirname, '../build/preload.js')).on('change', () => {
-            win.reload();
-        });
-    }
 }
 app.on('ready', createWindow)
--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './src/electron/preload.js',
-  output: {
-    filename: 'preload.js',
-    path: path.resolve(__dirname, 'build'),
-  },
-};
